46a30917bfa034dbda4661de73e0d581ac3cec8c
Chris Ahn <chrisahn1007@gmail.com>
2025-10-26 16:18:14 +0900

    feat(components-teamMatching): TeamCard 컴포넌트 퍼블리싱

---
+++ src/components/TeamMatching/TeamMatchingBody.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import TeamCard from './TeamCard';
+
+const TeamMatchingBody = () => {
+  return (
+    <div className="w-full h-full mt-8">
+      <div
+        className="w-[40%] h-full p-[47px]"
+        style={{
+          background: 'var(--1, linear-gradient(180deg, #16202B 0%, #1E2C33 50.01%, #203636 100%))',
+        }}
+      >
+        <TeamCard />
+      </div>
+    </div>
+  );
+};
+
+export default TeamMatchingBody;
--- src/pages/TeamMatching/TeamMatching.tsx
+++ src/pages/TeamMatching/TeamMatching.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import TeamMatchingHeader from '../../components/TeamMatching/TeamMatchingHeader';
+import TeamMatchingBody from '../../components/TeamMatching/TeamMatchingBody';
 
 const TeamMatching = () => {
   return (
     <div className="w-full h-full flex flex-col">
       <TeamMatchingHeader />
+      <TeamMatchingBody />
     </div>
   );
 };
