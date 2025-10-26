003783e53e0339d2a9de739705a947b06157615e
Chris Ahn <chrisahn1007@gmail.com>
2025-10-26 14:52:58 +0900

    feat(componenets-teammatching): TeamMatchingHeader 컴포넌트 구현

--- src/components/TeamMatching/TeamMatchingHeader.tsx
+++ src/components/TeamMatching/TeamMatchingHeader.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import RoundedSquareButton from '../commons/RoundedSquareButton';
 
 const TeamMatchingHeader = () => {
   return (
-    <div className="w-full h-[300px] flex flex-row justify-start items-center p-[47px]">
+    <div className="relative w-full h-[300px] flex flex-row justify-start items-center p-[47px] bg-[#16202b]">
       <div className="w-[70%] flex flex-col items-start gap-[10px]">
-        <h1 className="font-bold text-[64px]">Team Matching</h1>
+        <h1 className="font-[700] text-[64px]">Team Matching</h1>
         <h3 className="font-[600] text-[32px] text-left">
           PM들의 프로젝트 아이디어를 살펴보고, 함께 하고싶은 프로젝트에 지원해보세요!
         </h3>
       </div>
-      <div className="w-[30%]">버튼</div>
+      <div className="w-[30%] flex flex-row justify-end">
+        <RoundedSquareButton text={'아이디어 등록하기'} />
+      </div>
+      {/* TODO : PM 계정만 이 요소 보이도록 수정 */}
+      <div className="absolute bottom-[10px] right-[10px] font-[700] text-[24px] cursor-pointer">
+        지원결과 확인 →
+      </div>
     </div>
   );
 };
--- src/components/commons/RoundedSquareButton.tsx
+++ src/components/commons/RoundedSquareButton.tsx
@@ -2,7 +2,7 @@ const RoundedSquareButton = ({ text }: { text: string }) => {
   return (
     <button
       className="px-[45px] py-[20px] rounded-[10px] border border-solid border-border 
-        hover:bg-main text-white hover:text-[#18242D] absolute top-[65px] right-[70px]"
+        hover:bg-main text-white hover:text-[#18242D]"
     >
       <span className="text-24 font-700">{text}</span>
     </button>
