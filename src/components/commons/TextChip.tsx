1787591c140ef3518faf7ec456db10898f3f51a0
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-11-03 22:02:36 +0900

    fix(TextChip): 클릭했을 때 폼 입력 안되게 수정

--- src/components/commons/TextChip.tsx
+++ src/components/commons/TextChip.tsx
@@ -7,6 +7,7 @@ interface TextChipProps {
 const TextChip = ({ text, isSelected, onClick }: TextChipProps) => {
   return (
     <button
+      type="button"
       className={`px-24 py-12 rounded-full text-20 font-700 ${isSelected ? 'bg-main text-gray border-2 border-main' : 'border-solid border-2 border-border bg-gray text-white'}`}
       onClick={onClick}
     >
