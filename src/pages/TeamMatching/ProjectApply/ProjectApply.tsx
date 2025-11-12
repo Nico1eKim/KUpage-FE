eda5e6e98e26a1f3a9c60e0f510726c77a565a92
Chris Ahn <chrisahn1007@gmail.com>
2025-11-12 16:55:57 +0900

    ui(/components/ProjectApply): ProjectApplyHeader 컴포넌트 퍼블리싱

--- src/pages/TeamMatching/ProjectApply/ProjectApply.tsx
+++ src/pages/TeamMatching/ProjectApply/ProjectApply.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import ProjectApplyHeader from '../../../components/TeamMatching/ProjectApply/ProjectApplyHeader';
 
 const ProjectApply = () => {
-  return <div>ProjectApply</div>;
+  return (
+    <div className="h-full [background:var(--1,linear-gradient(180deg,#16202B_0%,#1E2C33_50.01%,#203636_100%))]">
+      <ProjectApplyHeader />
+    </div>
+  );
 };
 
 export default ProjectApply;
