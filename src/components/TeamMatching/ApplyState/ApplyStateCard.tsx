9fe53d79d509a4a8badc7d18567f8bdd54734cc3
Chris Ahn <chrisahn1007@gmail.com>
2025-11-18 23:50:31 +0900

    ui(ApplyStateCard): ApplyStateCard 퍼블리싱

--- src/components/TeamMatching/ApplyState/ApplyStateCard.tsx
+++ src/components/TeamMatching/ApplyState/ApplyStateCard.tsx
@@ -1,17 +1,72 @@
+import clsx from 'clsx';
 import TextBadge from '../../commons/TextBadge';
+
+const mockData = [
+  {
+    id: 0,
+    label: 'Android',
+    val: 2,
+  },
+  {
+    id: 1,
+    label: 'iOS',
+    val: 5,
+  },
+  {
+    id: 2,
+    label: 'Android',
+    val: 2,
+  },
+  {
+    id: 3,
+    label: 'Android',
+    val: 0,
+  },
+  {
+    id: 4,
+    label: 'Android',
+    val: 2,
+  },
+];
 
 const ApplyStateCard = () => {
   return (
-    <div className="w-full h-200 flex flex-row border-2 border-solid rounded-10 border-border">
-      <div className="w-[50%] flex flex-col gap-12 px-28 py-24 bg-gray rounded-l-10 text-left">
+    <div className="group w-full h-200 flex flex-row border-2 border-solid rounded-10 border-border cursor-pointer hover:border-main">
+      <div className="w-[50%] flex flex-col gap-12 px-28 py-24 bg-gray rounded-l-10 text-left group-hover:bg-main group-hover:text-darkblue">
         <h2 className="text-32 font-600">Chat Diary</h2>
         <h3 className="text-24 font-700">아아아아아</h3>
         <div className="flex flex-row items-center justify-between">
           <span className="text-24 font-500">skdlnsdf</span>
-          <TextBadge text={'Android'} />
+          <TextBadge text={'Android'} className="group-hover:border-darkblue" />
         </div>
       </div>
-      <div></div>
+      <div className="w-[50%] flex flex-row items-center gap-12 p-20 group-hover:bg-main/25">
+        {mockData.map((data) => {
+          return (
+            <div
+              key={data.id}
+              className="flex-1 min-w-0 h-full p-16 flex flex-col justify-end items-center bg-gray rounded-10 group-hover:bg-main group-hover:text-darkblue"
+            >
+              <span
+                className={clsx(
+                  'text-48 font-700',
+                  data.val === 0 && 'text-[#757575] group-hover:text-[rgba(52,52,52,0.37)]'
+                )}
+              >
+                {data.val}
+              </span>
+              <span
+                className={clsx(
+                  'text-20 font-700',
+                  data.val === 0 && 'text-[#757575] group-hover:text-[rgba(52,52,52,0.37)]'
+                )}
+              >
+                {data.label}
+              </span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/components/commons/TextBadge.tsx
+++ src/components/commons/TextBadge.tsx
@@ -1,13 +1,21 @@
+import clsx from 'clsx';
+
 interface Props {
   text: string;
+  className?: string;
 }
 
 /**
  * 그냥 텍스트만 표시하는 단순한 뱃지
  */
-const TextBadge = ({ text }: Props) => {
+const TextBadge = ({ text, className }: Props) => {
   return (
-    <div className="inline-flex self-start items-center justify-center h-[48px] px-24 rounded-24 border-[2px]">
+    <div
+      className={clsx(
+        'border-border inline-flex self-start items-center justify-center h-[48px] px-24 rounded-24 border-[2px] text-20 font-700',
+        className
+      )}
+    >
       {text}
     </div>
   );
