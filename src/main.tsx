375e3b50fafcce12a9d194d293bba2020fe01525
Chris Ahn <chrisahn1007@gmail.com>
2025-11-19 20:16:46 +0900

    chore: msw 셋팅

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,13 @@ import App from './App.tsx';
 import './assets/fonts/font.css';
 import './index.css';
 
+if (import.meta.env.MODE === 'development') {
+  const { worker } = await import('./mocks/browser');
+  await worker.start({
+    onUnhandledRequest: 'bypass', // mock하지 않은 요청은 그냥 서버로 흘려보냄
+  });
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
