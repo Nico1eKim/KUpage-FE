bcd64374cbc07975aa8135b6260fa2c87fb302db
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-11-22 12:43:33 +0900

    ui(Building.tsx): 제작 중 페이지 완성

---
+++ src/pages/Building/Building.tsx
@@ -0,0 +1,21 @@
+import FixIcon from '../../assets/imgs/Fix.svg?react';
+
+const Building = () => {
+  return (
+    <div
+      className="w-full h-[calc(100vh-141px)] text-white flex-center flex-col"
+      style={{
+        background: 'var(--1, linear-gradient(180deg, #16202B 0%, #1E2C33 50.01%, #203636 100%))',
+      }}
+    >
+      <h1 className="font-700 text-48">더 멋진 페이지를 만들고 있어요!</h1>
+      <p className="text-24 pt-32 pb-60">
+        이 페이지는 아직 공사 중이에요.
+        <br />곧 알찬 내용으로 꽉 채워서 찾아올게요!
+      </p>
+      <FixIcon className="size-240" />
+    </div>
+  );
+};
+
+export default Building;
