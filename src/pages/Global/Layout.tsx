3b43cdfc5f74e1b0db2765abbeb1aced20ad2237
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-11-29 17:20:59 +0900

    [refactor]: 등록하는 화면들은 header 상단고정되게 수정

--- src/pages/Global/Layout.tsx
+++ src/pages/Global/Layout.tsx
@@ -3,6 +3,7 @@ import { matchPath, Outlet, useLocation } from 'react-router-dom';
 
 import Footer from '../../components/global/Footer';
 import Header from '../../components/global/Header';
+import { MOVABLE_PATHS } from '../../constants/MovablePathsConstants';
 
 interface RoutesChildren {
   routes_children: {
@@ -19,11 +20,12 @@ const Layout = ({ routes_children }: RoutesChildren) => {
   const currentRoute = routes_children.find((child) => matchPath(child.path, pathname));
   const hasHeader = currentRoute?.hasHeader;
 
-  const mainContentPaddingClass = hasHeader ? 'pt-[141px]' : '';
+  const isMovable = MOVABLE_PATHS.some((path) => pathname === path);
+  const mainContentPaddingClass = hasHeader && !isMovable ? 'pt-[141px]' : '';
 
   return (
     <div id="layout" className="h-screen w-full md:overflow-x-hidden md:h-screen md:w-screen">
-      {hasHeader && <Header />}
+      {hasHeader && <Header isMovable={isMovable} />}
 
       <div className={mainContentPaddingClass}>
         <Suspense fallback={<div>Loading...</div>}>
