c0b73c0b9c30b5dc9ce0c92e685bef19403fc1cb
Chris Ahn <chrisahn1007@gmail.com>
2025-11-29 16:02:30 +0900

    hotfix(TeamMatching): 로그인 안했을시 팀매칭 페이지 접근시 로그인 페이지로 리다이렉션

--- src/hooks/useTeamMatching.ts
+++ src/hooks/useTeamMatching.ts
@@ -7,12 +7,8 @@ const useTeamMatching = () => {
   const { api } = useApi();
 
   const getTeamDatas = async () => {
-    try {
-      const res = await api.get<ApiResponse<TeamsResponse>>(ENDPOINTS.TEAMS);
-      return res.data;
-    } catch {
-      throw new Error('팀 데이터 불러오기 실패');
-    }
+    const res = await api.get<ApiResponse<TeamsResponse>>(ENDPOINTS.TEAMS);
+    return res.data;
   };
 
   return getTeamDatas;
