eeadb6f10e26af5482ad7086ae72a5ffc46f4cb0
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-11-29 23:40:22 +0900

    [hotfix]: 회원가입 리다이렉트 잘 되게 수정

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import App from './App.tsx';
 import './assets/fonts/font.css';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter } from 'react-router-dom';
 
 if (import.meta.env.MODE === 'development') {
   const { worker } = await import('./mocks/browser');
@@ -21,6 +22,8 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <BrowserRouter basename="/KUpage-FE">
+      <App />
+    </BrowserRouter>
   </QueryClientProvider>
 );
