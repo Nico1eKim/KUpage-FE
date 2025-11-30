c9029e80e9220778d32b2fb26a7a9c6953485380
Chris Ahn <chrisahn1007@gmail.com>
2025-11-30 20:32:11 +0900

    hotfix: 에러메시지 수정

--- src/components/TeamMatching/ProjectApply/schema/projectApplySchema.ts
+++ src/components/TeamMatching/ProjectApply/schema/projectApplySchema.ts
@@ -9,9 +9,9 @@ export const projectApplySchema = z.object({
     error: '지원 파트를 선택해주세요',
   }),
   portfolioUrl: z
-    .instanceof(File, { message: 'pdf 파일을 업로드해주세요.' })
-    .refine((f) => f.type === 'application/pdf', 'PDF 파일만 업로드할 수 있습니다.')
-    .refine((f) => f.size <= 10 * 1024 * 1024, '10MB 이하만 가능합니다.'),
+    .instanceof(File, { message: 'pdf 파일을 업로드해주세요' })
+    .refine((f) => f.type === 'application/pdf', 'PDF 파일만 업로드할 수 있습니다')
+    .refine((f) => f.size <= 10 * 1024 * 1024, '10MB 이하만 가능합니다'),
 });
 
 export type ProjectApplySchema = z.infer<typeof projectApplySchema>;
